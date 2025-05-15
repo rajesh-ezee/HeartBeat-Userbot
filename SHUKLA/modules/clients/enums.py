ddabb64631addef3b445799d22524a9beefe8d44
Rajesh Raki's <91279429+rajeshrakis@users.noreply.github.com>
2025-05-15 21:23:51 +0530

    enums.py

--- SHUKLA/modules/clients/enums.py
+++ SHUKLA/modules/clients/enums.py
@@ -45,7 +45,7 @@ async def run_async_enums():
                         check.pop(str(user_id))
                         flood.update({user_id: 0})
                     return await client.block_user(user_id)
-                pm_security = "**🤖 SECURITY WARNING ‼️ ({}/{})**".format(flood[str(user_id)], limits)
+                pm_security = "**ᴘᴍ ᴡᴀʀɴɪɴɢ ({}/{})🚨**".format(flood[str(user_id)], limits)
                 pm_permit_text = pm_security + "\n\n" + permit_text
                 try:
                     msg_dlt = await message.reply_photo(
