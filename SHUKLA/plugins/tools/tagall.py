9ffec7075055b0393cedbb72c1e7a0fb242e7d0b
Rajesh Raki's <91279429+rajeshrakis@users.noreply.github.com>
2025-05-15 22:18:31 +0530

    tagall.py

--- SHUKLA/plugins/tools/tagall.py
+++ SHUKLA/plugins/tools/tagall.py
@@ -67,7 +67,7 @@ __MENU__ = """
 `.gntop` - **to stop tagger**
 `.gmtag` - **good morning tagall**
 `.gmstop` - **to stop tagger**
-`.shayari` - **good night msg tagall**
-`.shayaristop` - **to stop tagger**
+`.hbtag` - **good night msg tagall**
+`.hbstop` - **to stop tagger**
 
 """
